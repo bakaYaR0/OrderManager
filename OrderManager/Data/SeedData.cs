7d2134b8dfd54b04807a59a91d3eeae326b23141
bakaYar0 <fpa.aff.air@gmail.com>
2022-04-01 19:02:03 +0300

    Seeding TestData for "Providers" to the DB

---
+++ OrderManager/Data/SeedData.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+using OrderManager.Models;
+using System;
+using System.IO;
+using System.Linq;
+
+namespace OrderManager.Data
+{
+    public static class SeedData
+    {
+        public static void SeedProviders(IServiceProvider serviceProvider)
+        {
+            using (var context = new OrderContext(
+                serviceProvider.GetRequiredService<DbContextOptions<OrderContext>>()))
+            {
+                if (context.Providers.Any())
+                {
+                    return;
+                }
+
+                var providersJson = File.ReadAllText(@".\\Data\\TestData.json");
+                Provider[] providers = JsonConvert.DeserializeObject<Provider[]>(providersJson);
+
+                context.Providers.AddRange(providers);
+                context.SaveChanges();
+            }
+        }
+    }
+}
