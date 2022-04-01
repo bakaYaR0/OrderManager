e7358d8133cb9a4f3025c775a268db58999f26e4
bakaYar0 <fpa.aff.air@gmail.com>
2022-04-01 18:41:16 +0300

    Adding EntityFrameworkCore and setting up the DB

---
+++ OrderManager/Data/OrderContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OrderManager.Models;
+
+namespace OrderManager.Data
+{
+    public class OrderContext : DbContext
+    {
+        public OrderContext(DbContextOptions<OrderContext> options) : base(options)
+        { }
+
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> Items { get; set; }
+        public DbSet<Provider> Providers { get; set; }
+    }
+}
