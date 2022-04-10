ee0a812c830327d169469996bd36eb44578b5728
bakaYar0 <fpa.aff.air@gmail.com>
2022-04-10 22:35:09 +0300

    Creating OrderView class to simplify displaying Items

---
+++ OrderManager/Models/OrderView.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace OrderManager.Models
+{
+    public class OrderView
+    {
+        public int Id { get; set; }
+        public string Number { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime Date { get; set; }
+
+        [Display(Name = "Provider Id")]
+        public int ProviderId { get; set; }
+
+        [Display(Name = "Provider")]
+        public string ProviderName { get; set; }
+        public List<OrderItem> ItemList { get; set; }
+
+        public OrderView()
+        {
+
+        }
+
+        public OrderView(Order order)
+        {
+            Id = order.Id;
+            Number = order.Number;
+            Date = order.Date;
+            ProviderId = order.ProviderId;
+        }
+
+        public OrderView(Order order, Provider provider, List<OrderItem> items)
+        {
+            Id = order.Id;
+            Number = order.Number;
+            Date = order.Date;
+            ProviderId = order.ProviderId;
+            ProviderName = provider.Name;
+            ItemList = items;
+        }
+    }
+}
