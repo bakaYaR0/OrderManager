6a6fc3c4c125ddc43579bba85f842b8ba76ce35e
bakaYar0 <fpa.aff.air@gmail.com>
2022-04-13 18:37:59 +0300

    this was supposed to be the main commit (Create and Edit orders, Filtration)

--- OrderManager/Controllers/OrdersController.cs
+++ OrderManager/Controllers/OrdersController.cs
@@ -20,9 +20,50 @@ namespace OrderManager.Controllers
         }
 
         // GET: Orders
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(string provider, string search, DateTime from, DateTime to)
         {
-            return View(await _context.Orders.ToListAsync());
+            var orders = await _context.Orders.ToListAsync();
+            var providers = await _context.Providers.Distinct().ToListAsync();
+            var items = await _context.Items.ToListAsync();
+
+            ViewBag.Providers = new SelectList(providers, "Id", "Name", provider);
+
+            var orderViews = new List<OrderView>();
+            foreach (var order in orders)
+            {
+                var orderView = new OrderView(order, providers.Where(x => x.Id == order.ProviderId).FirstOrDefault(), items);
+                orderViews.Add(orderView);
+            }
+
+            var orderQuery = orderViews.Select(x => x);
+
+            if (!string.IsNullOrEmpty(search))
+            {
+                ViewBag.Search = search;
+                orderQuery = orderQuery.Where(s => s.Number.Contains(search) ||
+                                                    items.Select(x => x.Name).Contains(search));
+            }
+
+            if (!string.IsNullOrEmpty(provider) && provider != "0")
+            {
+                orderQuery = orderQuery.Where(s => s.ProviderId.ToString() == provider);
+            }
+
+            if (from != DateTime.MinValue || to != DateTime.MinValue)
+            {
+                ViewBag.DateFrom = from.Date.ToString("yyyy-MM-dd");
+                ViewBag.DateTo = to.Date.ToString("yyyy-MM-dd");
+                orderQuery = orderQuery.Where(s => s.Date.CompareTo(from) >= 0 && s.Date.CompareTo(to) <= 0);
+            }
+            else
+            {
+                ViewBag.DateFrom = DateTime.Now.AddDays(-30).Date.ToString("yyyy-MM-dd");
+                ViewBag.DateTo = DateTime.Now.Date.ToString("yyyy-MM-dd");
+            }
+
+            orderViews = orderQuery.ToList();
+
+            return View(orderViews);
         }
 
         // GET: Orders/Details/5
@@ -35,35 +76,64 @@ namespace OrderManager.Controllers
 
             var order = await _context.Orders
                 .FirstOrDefaultAsync(m => m.Id == id);
+
             if (order == null)
             {
                 return NotFound();
             }
 
-            return View(order);
+            var provider = await _context.Providers.FirstOrDefaultAsync(m => m.Id == order.ProviderId);
+            var items = await _context.Items.Where(m => m.OrderId == order.Id).ToListAsync();
+            var orderView = new OrderView(order, provider, items);
+
+            return View(orderView);
         }
 
         // GET: Orders/Create
         public IActionResult Create()
         {
-            return View();
+            ViewBag.Providers = new SelectList(_context.Providers.Distinct(), "Id", "Name");
+
+            var orderView = new OrderView();
+            return View(orderView);
         }
 
         // POST: Orders/Create
         [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,ProviderId,Number,Date")] Order order)
+        public async Task<ActionResult> CreateOrder(Order order, List<OrderItem> items)
         {
-            if (ModelState.IsValid)
+            _context.Orders.Add(order);
+
+            try
             {
-                _context.Add(order);
                 await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
             }
-            return View(order);
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+
+            foreach (var item in items)
+            {
+                item.OrderId = order.Id;
+            }
+
+            _context.Items.AddRange(items);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+            return Json(new { redirectToUrl = Url.Action("Index", "Orders") });
         }
 
         // GET: Orders/Edit/5
+        [HttpGet]
         public async Task<IActionResult> Edit(int? id)
         {
             if (id == null)
@@ -72,44 +142,65 @@ namespace OrderManager.Controllers
             }
 
             var order = await _context.Orders.FindAsync(id);
+
             if (order == null)
             {
                 return NotFound();
             }
-            return View(order);
+            var providers = _context.Providers.Distinct().ToList();
+            var provider = providers.FirstOrDefault(m => m.Id == order.ProviderId);
+
+            var items = await _context.Items.Where(m => m.OrderId == order.Id).ToListAsync();
+
+            var orderView = new OrderView(order, provider, items);
+            ViewBag.Providers = new SelectList(providers, "Id", "Name");
+
+            return View(orderView);
         }
 
         // POST: Orders/Edit/5
         [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,ProviderId,Number,Date")] Order order)
+        public async Task<IActionResult> Edit(Order order, List<OrderItem> items)
         {
-            if (id != order.Id)
+            if (order.Id == 0)
             {
                 return NotFound();
             }
+            _context.Orders.Update(order);
 
-            if (ModelState.IsValid)
+            foreach (var item in items)
             {
-                try
-                {
-                    _context.Update(order);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!OrderExists(order.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
+                item.OrderId = order.Id;
             }
-            return View(order);
+
+            var newItems = items.Where(x => x.Id == 0);
+            var updatedItems = items.Where(x => x.Id != 0);
+
+            var orderItems = _context.Items.Where(x => x.OrderId == order.Id);
+            var deletedItems = orderItems.Where(x => !items.Contains(x));
+
+            try
+            {
+                _context.Orders.Update(order);
+                _context.Items.AddRange(newItems);
+                _context.Items.RemoveRange(deletedItems);
+                _context.Items.UpdateRange(updatedItems);
+
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!OrderExists(order.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return Json(new { redirectToUrl = Url.Action("Index", "Orders") });
         }
 
         // GET: Orders/Delete/5
@@ -136,7 +227,11 @@ namespace OrderManager.Controllers
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
             var order = await _context.Orders.FindAsync(id);
+            var orderItems = _context.Items.Where(x => x.OrderId == order.Id);
+
             _context.Orders.Remove(order);
+            _context.Items.RemoveRange(orderItems);
+
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
@@ -144,6 +239,11 @@ namespace OrderManager.Controllers
         private bool OrderExists(int id)
         {
             return _context.Orders.Any(e => e.Id == id);
+        }
+
+        public ActionResult DisplayNewItem()
+        {
+            return PartialView("_Item", new OrderItem());
         }
     }
 }
