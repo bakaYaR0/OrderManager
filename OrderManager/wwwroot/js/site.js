bd4e0a2c525b799b0423a32e9fe61ba4f10165f0
bakaYar0 <fpa.aff.air@gmail.com>
2022-04-13 18:35:35 +0300

    Adding ajax requests for front end html generation and data posting

--- OrderManager/wwwroot/js/site.js
+++ OrderManager/wwwroot/js/site.js
@@ -1,4 +1,84 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿{
+    $(document).on('click', '#Btn_AddItemSet', function (e) {
+        $.ajax({
+            url: '/Orders/DisplayNewItem',
+            success: function (partialView) {
+                $('.AddNewItem').append(partialView);
+            }
+        });
+    });
 
-// Write your JavaScript code.
+    $(document).on("click", "#Btn_DeleteItem", function () {
+        $(this).parent().parent().remove();
+    });
+
+    function getOrder() {
+        var order = {
+
+            Id: $("#OrderId").val(),
+            ProviderId: $("#OrderProvider").val(),
+            Date: $("#OrderDate").val(),
+            Number: $("#OrderNumber").val()
+        };
+
+        return order;
+    }
+
+    function getItemSets() {
+        itemSets = [];
+
+        const itemIds = document.querySelectorAll('#ItemId');
+        const itemNames = document.querySelectorAll('#ItemName');
+        const itemQuantities = document.querySelectorAll('#ItemQuantity');
+        const itemUnits = document.querySelectorAll('#ItemUnit');
+
+        for (var i = 0; i < itemNames.length; i++) {
+            if (itemNames[i].value != '') {
+                itemSets.push({
+                    Id: itemIds[i].value,
+                    Name: itemNames[i].value,
+                    Quantity: itemQuantities[i].value,
+                    Unit: itemUnits[i].value
+                });
+            }
+        }
+
+        return itemSets;
+    }
+
+    $(document).on('click', '#Btn_Create', function (e) {
+
+        var order = getOrder();
+        var items = getItemSets();
+
+        $.ajax({
+            type: 'POST',
+            data: { order, items },
+            url: '/Orders/CreateOrder',
+            success: function (response) {
+                window.location.href = response.redirectToUrl;
+            }
+        });
+    });
+
+    jQueryAjaxPost = form => {
+
+        var order = getOrder();
+        var items = getItemSets();
+
+        try {
+            $.ajax({
+                type: 'POST',
+                url: form.action,
+                data: { order, items },
+                success: function (response) {
+                    window.location.href = response.redirectToUrl;
+                }
+            });
+            //to prevent default form submit event
+            return false;
+        } catch (ex) {
+            console.log(ex)
+        }
+    }
+}
